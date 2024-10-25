69ad1c375ff273dab5e931494126af7bb2cbdfc7
ls-taylor-lee <117108016+ls-taylor-lee@users.noreply.github.com>
2024-10-25 14:11:45 -0400

    Revert middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import { routing } from "@/i18n/routing";
 import createMiddleware from "next-intl/middleware";
-import { locales } from "./constants/locale";
 
 export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", `/(${locales.join("|")})/:path*`],
+  matcher: ["/", "/(en|fr)/:path*"],
 };
